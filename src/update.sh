cb73222920708e31ec4ce1e679213d694efa7c09
Hans-Christian Knolle <hanscknolle08@gmail.com>
2025-02-10 18:29:50 +0100

    Added update.sh and License

---
+++ src/update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Setze die Pfade
+temp_dir="/home/temp"
+repo_url="https://github.com/HansKnolle08/gameandmore.eu.git"
+web_dir="/var/www/html"
+update_dir="/var/www/temp"
+
+# Alte Daten aus temp entfernen
+rm -rf "$temp_dir/gameandmore.eu"
+
+# Repository klonen
+mkdir -p "$temp_dir"
+git clone "$repo_url" "$temp_dir/gameandmore.eu"
+
+# Update-Verzeichnis vorbereiten
+rm -rf "$update_dir"
+mkdir -p "$update_dir"
+
+# Dateien und Ordner aus src in das Update-Verzeichnis verschieben
+mv "$temp_dir/gameandmore.eu/LICENSE" "$update_dir/"
+mv "$temp_dir/gameandmore.eu/src/index.html" "$update_dir/"
+mv "$temp_dir/gameandmore.eu/src/res" "$update_dir/"
+
+# Prüfen, ob sich die Dateien geändert haben
+if diff -qr "$web_dir" "$update_dir" > /dev/null; then
+    echo "Keine Änderungen gefunden. Bereinige Update-Verzeichnis."
+    rm -rf "$update_dir"
+else
+    echo "Änderungen erkannt. Aktualisiere Webseite."
+    rm -rf "$web_dir"
+    mv "$update_dir" "$web_dir"
+    chown -R www-data:www-data "$web_dir"
+    chmod -R 755 "$web_dir"
+fi
+
+# Temp-Verzeichnis bereinigen
+rm -rf "$temp_dir"
+
+# Cleanup
+echo "Update abgeschlossen."
